b97ffae8e5a736f27cc8d3f76658714f30622a79
CecilianaCruz <152406764+CecilianaCruz@users.noreply.github.com>
2024-03-28 16:05:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal('.imagem', 
+{
+    duration:2000,
+    distance: '150px',
+    delay: 500,
+    origin: 'right'
+});
+
+
+revelar.reveal('.text', 
+{
+    duration:2500,
+    distance: '90px',
+});
+
+
+
+revelar.reveal('.partetwo', 
+{
+    duration:1000,
+    distance: '90px',
+});
+
+revelar.reveal('.texttre', 
+{
+    duration:1000,
+    distance: '100px',
+});
+
+revelar.reveal('.card', 
+{
+    duration:1000,
+    distance: '100px',
+});
+
+revelar.reveal('.aboutimg', 
+{
+    duration:1000,
+    distance: '100px',
+});
+
+revelar.reveal('.abouttext', 
+{
+    duration:1000,
+    distance: '100px',
+});
+
